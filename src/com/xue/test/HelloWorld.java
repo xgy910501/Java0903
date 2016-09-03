ecf23791f9ba6d10c36d0255753afeee2da1f969
xgy910501 <420617921@qq.com>
2016-09-03 12:38:15 +0800

    Initial commit

---
+++ src/com/xue/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xue.test;
+
+/**
+ * Created by Administrator on 2016/9/3.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.print("Hello World!!");
+    }
+}
